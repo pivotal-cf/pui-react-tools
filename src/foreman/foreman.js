d0cb6cfc68c01ae5c0b68fc85e42d24f2800ca37
Charles Hansen <chansen87@gmail.com>
2016-01-22 11:18:21 -0800

    Fix foreman task on windows

--- src/foreman/foreman.js
+++ src/foreman/foreman.js
@@ -3,11 +3,17 @@ const npm = require('npm');
 const path = require('path');
 const {spawn} = require('child_process');
 
+function crossPlatformSpawn(cmd, args, options) {
+    if(process.platform === 'win32') {
+        return spawn('cmd.exe', ['/c', cmd, ...args], options);
+    }
+    return spawn(cmd, args, options);
+}
+
 function foreman(callback) {
   npm.load(function(err) {
     if (err) return callback(err);
-    const child = spawn(path.join(npm.bin, 'nf'), ['start', '-j', 'Procfile.dev'], {stdio: 'inherit', env: process.env})
-      .once('close', callback);
+    const child = crossPlatformSpawn(path.join(npm.bin, 'nf'), ['start', '-j', 'Procfile.dev'], {stdio: 'inherit', env: process.env}).once('close', callback);   
     ['SIGINT', 'SIGTERM'].forEach(e => process.once(e, () => child && child.kill()));
   });
 }
