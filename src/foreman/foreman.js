eebbfc407491c4f6c6afa09b02b8f49f68ff22fb
Weyman Fung <wfung@pivotal.io>
2016-07-22 16:58:51 -0700

    expose foreman task so it can be overridden
    Signed-off-by: Ryan Dy <rdy@pivotal.io>

--- src/foreman/foreman.js
+++ src/foreman/foreman.js
@@ -10,19 +10,23 @@ function crossPlatformSpawn(cmd, args, options) {
     return spawn(cmd, args, options);
 }
 
-function foreman(callback) {
-  npm.load(function(err) {
-    if (err) {
-      callback(err);
-      return;
-    }
-    const child = crossPlatformSpawn(path.join(npm.bin, 'nf'), ['start', '-j', 'Procfile.dev'], {stdio: 'inherit', env: process.env}).once('close', callback);   
-    ['SIGINT', 'SIGTERM'].forEach(e => process.once(e, () => child && child.kill()));
-  });
-}
-
-module.exports = {
+const Foreman = {
   install() {
-    gulp.task('foreman', foreman);
+    gulp.task('foreman', Foreman.tasks.foreman);
+  },
+
+  tasks: {
+    foreman(callback) {
+      npm.load(function(err) {
+        if (err) {
+          callback(err);
+          return;
+        }
+        const child = crossPlatformSpawn(path.join(npm.bin, 'nf'), ['start', '-j', 'Procfile.dev'], {stdio: 'inherit', env: process.env}).once('close', callback);
+        ['SIGINT', 'SIGTERM'].forEach(e => process.once(e, () => child && child.kill()));
+      });
+    }
   }
 };
+
+module.exports = Foreman;
